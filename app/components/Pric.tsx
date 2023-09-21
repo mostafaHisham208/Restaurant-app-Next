42f0f631ecc917a4e81d597026dbf4f789e6c3a1
Mostafa Hisham <hmostafaharpy22@gmail.com>
2023-09-21 23:14:19 +0300

    Add products,productDetails  page

--- app/components/Pric.tsx
+++ app/components/Pric.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 
 type props={
     price:number;
@@ -7,25 +8,39 @@ type props={
 }
 
 const Pric = ({price,id,options}:props) => {
+    const [total,settotal]=useState(price);
+    const [quantity,setquantity]=useState(1);
+    const [selected,setselected]=useState(0);
+
+    useEffect(()=>{
+        settotal(quantity * (options ? price+options[selected].additionalPrice: price));
+    },[quantity,selected,options,price]);
   return (
-    <div>
-        <h2>${price.toFixed(2)}</h2>
+    <div className='flex flex-col gap-4'>
+        <h2 className='text-2xl font-bold'>${total.toFixed(2)}</h2>
         
-        <div className="flex gap-4 justify-between">
-            {options?.map((option)=>(
-                <button className='border-2  rounded-md p-3' key={option.title}>{option.title}</button>
+        <div className="flex gap-4">
+            {options?.map((option,index)=>(
+                <button 
+                className=' min-w-[6rem]   ring-2 ring-red-400 p-2   rounded-md'
+                 key={option.title}
+                 style={{background:selected === index ? "rgb(248 113 113)" : "white",
+                 color:selected === index ? "white" : "red"
+                }}
+                onClick={()=>setselected(index)}
+                 >{option.title}</button>
             ))}
         </div>
         <div className="flex w-full items-center mt-2">
-            <div className='w-1/2 flex justify-between border-red-500 border-2 p-4'>
+            <div className='w-full flex justify-between border-red-500 border-2 p-3'>
                <span>Quantity</span>
-               <div className="">
-                <button>{'<'}</button>
-                <span className='px-1'>1</span>
-                <button>{'>'}</button>
+               <div className="flex gap-4 items-center">
+                <button onClick={()=>setquantity(prev=>(prev>1 ? prev-1 :1))}>{'<'}</button>
+                <span className=''>{quantity}</span>
+                <button onClick={()=>setquantity(prev=>(prev<9 ? prev+1 :1))}>{'>'}</button>
                 </div> 
             </div>
-            <button className='bg-red-500 text-white w-1/2 p-4'>Add to Cart</button>
+            <button className='bg-red-500 text-white w-56   ring-1 ring-red-500 p-3'>Add to Cart</button>
         </div>
     </div>
   )
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -4,18 +4,25 @@ import Image from 'next/image'
 import React from 'react'
 
 const Singlproductpage = () => {
-  return (
-    <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row'>
-      {singleProduct.img &&<div className="relative">
-        <Image src={singleProduct.img} alt='' className='object-contain' fill/>
-      </div>  }   
-      <div>
-        <h1>{singleProduct.title}</h1>
-        <p>{singleProduct.desc}</p>
-        <Pric price={singleProduct.price} id={singleProduct.id} options={singleProduct.options}/> 
-      </div>
-   </div>
-  )
+    return (
+        <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:items-center md:gap-8' >
+            {singleProduct.img && 
+            <div className="relative w-full h-1/2 md:h-[70%]">
+                <Image
+                    src={singleProduct.img}
+                    alt=''
+                    className='object-contain'
+                    fill
+                />
+            </div>
+            }
+            <div className='h-1/2 flex flex-col gap-4 md:h-[70%] justify-center md:gap-6  xl:gap-8'>
+                <h1 className='text-3xl font-bold uppercase xl:text-5xl'>{singleProduct.title}</h1>
+                <p>{singleProduct.desc}</p>
+                <Pric price={singleProduct.price} id={singleProduct.id} options={singleProduct.options} />
+            </div>
+        </div>
+    )
 }
 
 export default Singlproductpage
